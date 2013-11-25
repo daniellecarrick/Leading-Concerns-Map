eb6f6e9b872717f6574dd15c7ab878b881e1c015
daniellecarrick <dacarrick@gmail.com>
2013-11-24 23:06:34 -0500

    find and replace to add img and p to descriptions

--- Site/concernDescriptions.js
+++ Site/concernDescriptions.js
@@ -1,102 +1,102 @@
 var concernDescriptions = {
 	"2008" : {
 		"Recession and unemployment":{
-			"desc" : "<strong>2008 | Recession and unemployment</strong><p><strong><Recession and unemployment</strong> was #3 of 19 trended concerns in 2008, tied with having enough money. It was among the top five issues in 17 countries and was #1 in three: South Korea, Taiwan, and Thailand.</p>"
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Recession and unemployment</strong><p><strong><Recession and unemployment</strong> was #3 of 19 trended concerns in 2008, tied with having enough money. It was among the top five issues in 17 countries and was #1 in three: South Korea, Taiwan, and Thailand.</p>"
 		},
 		"Inflation and high prices":{
-			"desc" : "<strong>2008 | Inflation and high prices</strong><br /><strong>Inflation and high prices</strong> was the #1 concern globally in 2008, up 2 places from 2003, when it ranked 3rd. It was in the top five in 17 countries and was #1 in seven, including China, Germany, and France."
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Inflation and high prices</strong><br /><strong>Inflation and high prices</strong> was the #1 concern globally in 2008, up 2 places from 2003, when it ranked 3rd. It was in the top five in 17 countries and was #1 in seven, including China, Germany, and France.</p>"
 		},
 
 		"Money enough to live right and pay the bills":{
-			"desc" : "<strong>2008 | Money enough to live right and pay the bills</strong><p><strong>Having money enough to live right and pay the bills</strong> was tied for #3 globally in 2008 with recession and unemployment. It was in the top five in 15 countries and was #1 in one, Canada.</p>"
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Money enough to live right and pay the bills</strong><p><strong>Having money enough to live right and pay the bills</strong> was tied for #3 globally in 2008 with recession and unemployment. It was in the top five in 15 countries and was #1 in one, Canada.</p>"
 		},
 
 		"Crime and lawlessness":{
-			"desc" : "<strong>2008 | Crime and lawlessness</strong><br /><strong>Crime and lawlessness</strong> ranked 2nd in the world’s concerns in 2008. It was in the most pervasive concern, ranking in the top five in 23 of 25 countries and #1 in 10 countries, including the US, Brazil, and India."
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Crime and lawlessness</strong><br /><strong>Crime and lawlessness</strong> ranked 2nd in the world’s concerns in 2008. It was in the most pervasive concern, ranking in the top five in 23 of 25 countries and #1 in 10 countries, including the US, Brazil, and India.</p>"
 		},
 
 		"Environmental pollution":{
-			"desc" : "<strong>2008 | Environmental pollution</strong><br /><strong>Environmental pollution</strong> was the world’s 5th-ranked issue in 2008. It had the largest gain of any trended issue from 2003-2008, +3 ranks. It was among the top five concerns in nine countries and was #1 in two, Japan and Sweden. "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Environmental pollution</strong><br /><strong>Environmental pollution</strong> was the world’s 5th-ranked issue in 2008. It had the largest gain of any trended issue from 2003-2008, +3 ranks. It was among the top five concerns in nine countries and was #1 in two, Japan and Sweden.</p>"
 		},
 
 		"Wrongdoing by/corruption of elected officials":{
-			"desc" : "<strong>2008 | Wrongdoing by/corruption of elected officials</strong><p><strong>Wrongdoing and corruption</strong> on the part of elected government officials tied with terrorism for the 9th-ranked concern globally in 2008. It was in the top five in five countries. </p>"
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Wrongdoing by/corruption of elected officials</strong><p><strong>Wrongdoing and corruption</strong> on the part of elected government officials tied with terrorism for the 9th-ranked concern globally in 2008. It was in the top five in five countries. </p>"
 		},
 
 		"Educational quality":{
-			"desc" : "<strong>2008 | Educational quality</strong><br /><strong>Educational quality</strong> was the #6 issue globally in 2008. It was in the top five in 10 countries, ranking highest in Argentina, India, and Taiwan (#3 each)."
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Educational quality</strong><br /><strong>Educational quality</strong> was the #6 issue globally in 2008. It was in the top five in 10 countries, ranking highest in Argentina, India, and Taiwan (#3 each).</p>"
 		},
 
 		"Cost of health care":{
-			"desc" : "<strong>2008 | Cost of health care</strong><br />The <strong>cost of health care</strong> was the 8th-ranked issue globally in 2008. It was in the top five in three countries, the US, the Czech Republic, and Germany. "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Cost of health care</strong><br />The <strong>cost of health care</strong> was the 8th-ranked issue globally in 2008. It was in the top five in three countries, the US, the Czech Republic, and Germany. </p>"
 		},
 
 		"Future of retirement benefits provided by govt":{
-			"desc" : "<strong>2008 | Future of retirement benefits provided by govt</strong><p><strong>The future of retirement and pension plans</strong> funded by government ranked #13 of 19 trended issues globally in 2008. It was in the top five in two countries, Japan and Germany. </p>"
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Future of retirement benefits provided by govt</strong><p><strong>The future of retirement and pension plans</strong> funded by government ranked #13 of 19 trended issues globally in 2008. It was in the top five in two countries, Japan and Germany. </p>"
 		},
 
 		"Economic inequality":{
-			"desc" : "<strong>2008 | Economic inequality</strong><br /><strong>Economic inequality</strong> was the 14th-ranked issue globally in 2008, tied with immigration. It did not register in the top five in any of Roper Reports Worldwide’s core-25 countries.  "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Economic inequality</strong><br /><strong>Economic inequality</strong> was the 14th-ranked issue globally in 2008, tied with immigration. It did not register in the top five in any of Roper Reports Worldwide’s core-25 countries.</p>"
 		},
 
 		"Global climate change/global warming":{
-			"desc" : "<strong>2008 | Global climate change/global warming</strong><br /><strong>Global warming and climate change</strong> was the 11th-ranked worry globally in 2008, tied with AIDS. It was in the top five in three countries: Japan (#2), Sweden (#3), and Australia (#4)."
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Global climate change/global warming</strong><br /><strong>Global warming and climate change</strong> was the 11th-ranked worry globally in 2008, tied with AIDS. It was in the top five in three countries: Japan (#2), Sweden (#3), and Australia (#4).</p>"
 		},
 
 
 		"Terrorism":{
-			"desc" : "<strong>2008 | Terrorism</strong><br /><strong>Terrorism</strong> ranked #9 issue globally in 2008, tied with wrongdoing by government officials. But it was down markedly from 2003, when it ranked 5th. It was in the top five in five countries, led by Turkey (#1) and Spain (#2). "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Terrorism</strong><br /><strong>Terrorism</strong> ranked #9 issue globally in 2008, tied with wrongdoing by government officials. But it was down markedly from 2003, when it ranked 5th. It was in the top five in five countries, led by Turkey (#1) and Spain (#2). </p>"
 		},
 
 		"Drug abuse":{
-		"desc" : "<strong>2008 | Drug abuse</strong><p><strong>Drug abuse</strong> was the #7 concern globally in 2008. It ranked in the top five in 13 countries, led by Argentina, Brazil, and Mexico (#2 each). </p>"
+		"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Drug abuse</strong><p><strong>Drug abuse</strong> was the #7 concern globally in 2008. It ranked in the top five in 13 countries, led by Argentina, Brazil, and Mexico (#2 each). </p>"
 		},
 
 		"Religious extremism/fundamentalism":{
-			"desc" : "<strong>2008 | Religious extremism/fundamentalism</strong><br /><strong>Religious extremism and fundamentalism</strong> tied for #16 with relations between racial and ethnic groups in 2008. It did not make the top five in any country."
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Religious extremism/fundamentalism</strong><br /><strong>Religious extremism and fundamentalism</strong> tied for #16 with relations between racial and ethnic groups in 2008. It did not make the top five in any country.</p>"
 		},
 
 		"Immigration":{
-			"desc" : "<strong>2008 | Immigration</strong><br /><strong>Immigration</strong> tied for 14th with economic inequality in 2008. It ranked in the top five in one country, the UK (#2). "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Immigration</strong><br /><strong>Immigration</strong> tied for 14th with economic inequality in 2008. It ranked in the top five in one country, the UK (#2). </p>"
 		},
 
 	
 		"Relations with foreign countries":{
-			"desc" : "<strong>2008 | Relations with foreign countries</strong><br /><strong>Foreign relations</strong> ranked 18th of 19 concerns globally in 2008. It was not a top-five issue in any country. "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Relations with foreign countries</strong><br /><strong>Foreign relations</strong> ranked 18th of 19 concerns globally in 2008. It was not a top-five issue in any country. </p>"
 		},
 
 		"AIDS":{
-			"desc" : "<strong>2008 | AIDS</strong><br /><strong>AIDS</strong> was the world’s 11th-ranked worry in 2008, tied with climate change. It was among the top five issues in four countries, led by South Africa (#1) and Indonesia (#2). "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | AIDS</strong><br /><strong>AIDS</strong> was the world’s 11th-ranked worry in 2008, tied with climate change. It was among the top five issues in four countries, led by South Africa (#1) and Indonesia (#2).</p> "
 		},
 
 		"Relations between racial and ethnic groups":{
-			"desc" : "<strong>2008 | Relations between racial and ethnic groups</strong><br /><strong>Relations between racial and ethnic groups</strong> was the #16 issue in the world in 2008, tied with religious extremism/fundamentalism. It did not make the top five in any country. "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Relations between racial and ethnic groups</strong><br /><strong>Relations between racial and ethnic groups</strong> was the #16 issue in the world in 2008, tied with religious extremism/fundamentalism. It did not make the top five in any country.</p> "
 		},
 
 		"Aid to countries in Third World":{
-			"desc" : "<strong>2008 | Aid to countries in Third World</strong><br /><strong>Aid to countries in the Third World</strong> was last of 19 trended issues in 2008. It did not make the top five in any country. "
+			"desc" : "<img src='img/icon01.png'/><p><strong>2008 | Aid to countries in Third World</strong><br /><strong>Aid to countries in the Third World</strong> was last of 19 trended issues in 2008. It did not make the top five in any country. </p>"
 		}
 	},
 	
 	"2013" : {
 		"Recession and unemployment":{
-			"desc" : "<strong>2013 | Recession and unemployment</strong><br />Five years after the global financial crisis, concerns about <strong>recession and unemployment</strong> still weigh on the world. The issue ties with inflation and high prices as the #1 concern globally, up from #3 in 2008. Recession and unemployment is the most pervasive issue, ranking in the top five in 23 of 25 countries. It is #1 in 10 countries, including the US."
+			"desc" : "<img src='img/icon01.png'/><p><strong>2013 | Recession and unemployment</strong><br />Five years after the global financial crisis, concerns about <strong>recession and unemployment</strong> still weigh on the world. The issue ties with inflation and high prices as the #1 concern globally, up from #3 in 2008. Recession and unemployment is the most pervasive issue, ranking in the top five in 23 of 25 countries. It is #1 in 10 countries, including the US.</p>"
 		},
 
 		"Inflation and high prices":{
-			"desc":"<strong>2013 | Inflation and high prices</strong><br />The deal-mindedness consumers have shown since the global financial crisis is not going away. <strong>Inflation and high prices</strong> ties recession and unemployment as the world’s #1 concern. It ranks in the top five in 21 of 25 countries and is #1 in five countries, including China and Russia."
+			"desc":"<strong>2013 | Inflation and high prices</strong><br />The deal-mindedness consumers have shown since the global financial crisis is not going away. <strong>Inflation and high prices</strong> ties recession and unemployment as the world’s #1 concern. It ranks in the top five in 21 of 25 countries and is #1 in five countries, including China and Russia.</p>"
 		},
 
 		"Money enough to live right and pay the bills":{
-			"desc":"<strong>2013 | Money enough to live right and pay the bills</strong><br />Consumers’ preoccupation with finances extends to their 3rd-ranked concern. Having <strong>enough money to live right and pay bills</strong> is in the top five in 19 of 25 countries. It is #1 in two countries, Australia and Canada."
+			"desc":"<strong>2013 | Money enough to live right and pay the bills</strong><br />Consumers’ preoccupation with finances extends to their 3rd-ranked concern. Having <strong>enough money to live right and pay bills</strong> is in the top five in 19 of 25 countries. It is #1 in two countries, Australia and Canada.</p>"
 		},
 
 		"Crime and lawlessness":{
-			"desc":"<strong>2013 | Crime and lawlessness</strong><br />While the economy has crowded out <strong>crime and lawlessness</strong> globally – it has slipped from 2nd to 4th since 2008 – the issue continues to be paramount in some key developing countries. It is the #1 issue in eight countries, including Brazil, Mexico, and India. It ranks in the top five in 20 of 25 countries."
+			"desc":"<strong>2013 | Crime and lawlessness</strong><br />While the economy has crowded out <strong>crime and lawlessness</strong> globally – it has slipped from 2nd to 4th since 2008 – the issue continues to be paramount in some key developing countries. It is the #1 issue in eight countries, including Brazil, Mexico, and India. It ranks in the top five in 20 of 25 countries.</p>"
 		},
 
 		"Environmental pollution":{
-			"desc": "<strong>2013 | Environmental pollution</strong><br />Environmentalists have a point: It’s hard to focus the world’s attention on <strong>environmental pollution</strong> amid so many other problems. Pollution ranks #5 globally, but that is largely driven by its high rank in China (#2) and Sweden (#1). It ranks in the top five in only 7 of 25 countries."
+			"desc": "<strong>2013 | Environmental pollution</strong><br />Environmentalists have a point: It’s hard to focus the world’s attention on <strong>environmental pollution</strong> amid so many other problems. Pollution ranks #5 globally, but that is largely driven by its high rank in China (#2) and Sweden (#1). It ranks in the top five in only 7 of 25 countries.</p>"
 		},
 
 		"Wrongdoing by/corruption of elected officials":{
